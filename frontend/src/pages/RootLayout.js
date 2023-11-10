e26b36cdc9362eb80d239dd50918d30d0aee8360
mjvinti <mjvinti@gmail.com>
2023-11-10 10:01:29 -0500

    added automatic logout

--- frontend/src/pages/RootLayout.js
+++ frontend/src/pages/RootLayout.js
@@ -1,8 +1,22 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 
 const RootLayout = () => {
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    setTimeout(() => {
+      submit(null, { action: '/logout', method: 'POST' });
+    }, 1 * 60 * 60 * 1000);
+  }, [submit, token]);
+
   return (
     <>
       <MainNavigation />
